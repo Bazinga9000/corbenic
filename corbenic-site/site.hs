b827d5c1133dd9221a489173cfca8fd10606c83e
Bazinga9000 <19626297+Bazinga9000@users.noreply.github.com>
2026-08-29 22:07:11 -0400

    reorganize site, add first bit of spec

--- corbenic-site/site.hs
+++ corbenic-site/site.hs
@@ -3,9 +3,9 @@
 import           Hakyll
 import           System.Environment (lookupEnv)
 import           System.Process.Typed (proc, readProcess)
-import qualified Data.ByteString.Lazy.Char8 as LBS
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Lazy.Encoding as TLE
 import           Control.Monad (unless)
-import           Data.List (isPrefixOf)
 import           Data.Maybe (fromMaybe)
 
 --------------------------------------------------------------------------------
@@ -31,25 +31,26 @@ typstCompiler = do
         readProcess (proc "typst" ["compile", "--features", "html", "--format", "html", inputFile, "-"])
     let code = show exitCode
     unless ("ExitSuccess" `elem` words code) $
-        fail $ "typst failed for " ++ inputFile ++ ":\n" ++ LBS.unpack err
-    -- typst html exports a full document; extract only <body>
-    let html = LBS.unpack out
+        fail $ "typst failed for " ++ inputFile ++ ":\n" ++ show err
+    -- typst html exports a full document; extract only <body> and explicitly decode as UTF-8
+    let html = TLE.decodeUtf8 out :: TL.Text
         body = fromMaybe html (extractBetween "<body>" "</body>" html)
-    makeItem body
+    makeItem (TL.unpack body)
 
 -- extract the text between the first occurrence of open and the next close
-extractBetween :: String -> String -> String -> Maybe String
+extractBetween :: TL.Text -> TL.Text -> TL.Text -> Maybe TL.Text
 extractBetween open close s = do
     (_, afterOpen) <- findSub open s
     (beforeClose, _) <- findSub close afterOpen
     pure beforeClose
   where
-    findSub pat = go []
+    findSub pat = go TL.empty
       where
-        go _ [] = Nothing
-        go acc rest@(c:cs)
-            | pat `isPrefixOf` rest = Just (reverse acc, drop (length pat) rest)
-            | otherwise = go (c:acc) cs
+        go acc rest
+            | Just r <- TL.stripPrefix pat rest = Just (acc, r)
+            | otherwise = case TL.uncons rest of
+                Nothing -> Nothing
+                Just (c, cs) -> go (TL.snoc acc c) cs
 
 --------------------------------------------------------------------------------
 -- Main
@@ -57,8 +58,8 @@ extractBetween open close s = do
 
 main :: IO ()
 main = config >>= \cfg -> hakyllWith cfg $ do
-    match "CNAME" $ do
-        route   idRoute
+    match "static/**" $ do
+        route   (gsubRoute "static/" (const ""))
         compile copyFileCompiler
 
     match "*.typst" $ do
