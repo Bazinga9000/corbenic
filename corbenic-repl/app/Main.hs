543f71051b3400fed39e94969cadf5f87df23693
Bazinga9000 <19626297+Bazinga9000@users.noreply.github.com>
2026-08-31 21:58:38 -0400

    repl: add :l command to see lexer

--- corbenic-repl/app/Main.hs
+++ corbenic-repl/app/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Frontend.IOPipeline
 import System.Console.Haskeline
 
 main :: IO ()
@@ -12,6 +13,14 @@ main = runInputT defaultSettings loop
         case minput of
             Nothing -> pass
             Just ":q" -> pass
+            Just (':' : 'l' : ' ' : toLex) -> do
+                mToks <- ioLex "<stdin>" toLex
+                case mToks of
+                    Nothing -> loop
+                    Just toks -> putTextLn (prettyPrint toks) >> loop
+            Just (':' : rest) -> do
+                -- todo color
+                putStrLn $ "Invalid REPL command :" <> rest
             Just input -> do
                 putStrLn $ "You input: " <> input
                 loop
